b10e94b5d4fb0a5c3d3ef0fe17e23ab64b0c1b88
임준영 <juny@lguplus.co.kr>
2023-02-27 10:11:43 +0900

    변수 세팅
    Detail
    ======
     -

--- vars/onlinePipeline.groovy
+++ vars/onlinePipeline.groovy
@@ -1,4 +1,26 @@
-def call() {
-  def pipeline = loadPipeline()
-  pipeline()
+import com.lguplus.sre.online.TestAutomation
+
+def call(Map baseArgs) {
+    def SKIPTESTS_DEFAULT_VALUE = TestAutomation.getDefaultYN(baseArgs.project)
+    def TEST_DESCRIPTION = TestAutomation.getSkipTestDescription(baseArgs.project)
+
+    def PARAMETER_LIST = []
+
+    // parameters 정의
+    def skipTest = booleanParam(name: 'SKIPTESTS', defaultValue: SKIPTESTS_DEFAULT_VALUE, description: TEST_DESCRIPTION)
+
+    if (env.BRANCH_NAME == 'develop' || [/feature.*/, /hotfix.*/].any { env.BRANCH_NAME =~ it }) {
+        PARAMETER_LIST.add(skipTest)
+}
+
+    def newParams = parameters(PARAMETER_LIST)
+    properties([
+        newParams
+    ])
+
+    def pipelineArgs = [:]
+
+    pipeline {
+        agent any
+    }
 }
