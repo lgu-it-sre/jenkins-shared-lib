0a2e28309a3f34105d013c2fe0cd48fc8d1c721c
임준영 <juny@lguplus.co.kr>
2023-02-27 10:59:17 +0900

    Checkout source
    Detail
    ======
     -

--- vars/onlinePipeline.groovy
+++ vars/onlinePipeline.groovy
@@ -8,7 +8,9 @@ def call(Map baseArgs) {
 
     // parameters 정의
     def skipTest = booleanParam(name: 'SKIPTESTS', defaultValue: SKIPTESTS_DEFAULT_VALUE, description: TEST_DESCRIPTION)
+    def rollback = booleanParam(name: 'ROLLBACK', defaultValue: false, description: '체크 시 입력한 commit hash 값으로 checkout 합니다.')
 
+    PARAMETER_LIST.add(rollback)
     if (env.BRANCH_NAME == 'develop' || [/feature.*/, /hotfix.*/].any { env.BRANCH_NAME =~ it }) {
         PARAMETER_LIST.add(skipTest)
 }
@@ -22,5 +24,47 @@ def call(Map baseArgs) {
 
     pipeline {
         agent any
+
+        options {
+            disableConcurrentBuilds()
+            skipDefaultCheckout(true)
+            buildDiscarder(logRotator(numToKeepStr: '10'))
+        }
+
+        post {
+            always {
+                cleanWs(cleanWhenNotBuilt: true, deleteDirs: true, disableDeferredWipeout: false, notFailBuild: true)
+            }
+        }
+
+        stages {
+            stage('Checkout') {
+                steps {
+                    cleanWs()
+                    checkout scm
+
+                    script {
+                        def git_hash = sh(returnStdout: true, script: 'git rev-parse HEAD').trim()
+                        if (params.ROLLBACK) {
+                            timeout(time: 10, unit: 'MINUTES') {
+                                git_hash = input(
+                                    message: 'Rollback 할 commit hash 를 입력해주세요.',
+                                    parameters: [[
+                                        $class: 'TextParameterDefinition',
+                                        defaultValue: git_hash,
+                                        description: 'Enter commit hash',
+                                        name: 'commit_hash'
+                                    ]]
+                                ).trim()
+                                echo "Project Name: ${baseArgs.project}"
+                                echo "Choice: ${git_hash}"
+
+                                sh "git checkout ${git_hash}"
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
