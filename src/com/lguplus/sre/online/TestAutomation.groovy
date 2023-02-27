7120e142affea8fe5f8fd67f6784c49bc4d3d888
임준영 <juny@lguplus.co.kr>
2023-02-27 10:11:43 +0900

    변수 세팅
    Detail
    ======
     -

---
+++ src/com/lguplus/sre/online/TestAutomation.groovy
@@ -0,0 +1,13 @@
+package com.lguplus.sre.online
+
+class TestAutomation {
+
+    static String getDefaultYN(String appName) {
+        return true
+    }
+
+    static String getSkipTestDescription(String appName) {
+        return '선택 시 테스트를 건너뛰고 진행합니다.'
+    }
+
+}
